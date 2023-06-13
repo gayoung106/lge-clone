2a8f20f11663f1d646b0b03920d59959c478868a
SongGayoung <xjongam1x@gmail.com>
2023-06-13 15:59:52 +0900

    modified: nav아이템 map활용

--- src/organisms/HeaderNav.jsx
+++ src/organisms/HeaderNav.jsx
@@ -1,19 +1,52 @@
 import React from "react";
-import { HeaderMenu, NavList } from "../styles/Header";
+import {
+  HeaderMenu,
+  NavList,
+  NavItemTag,
+  CategoryLayer,
+  CategoryInner,
+  SwiperContainer,
+  SwiperWrap,
+  SwiperSlide,
+  SlideItemTag,
+} from "../styles/Header";
 import Signiture from "../assets/logo-signature.svg";
 import ObjetLg from "../assets/logo-objet-collection.svg";
 import ThinkQ from "../assets/logo-thinq.svg";
 import Gram from "../assets/logo-lets-gram.svg";
 
 const HeaderNav = () => {
+  // const navlist = [
+  //   {name:"제품/소모품", tagName:["TV/AV", "PC/모니터", "주방가전", "생활가전", "에어컨/에어케어", "뷰티/의료기기", "로봇", "LG SIGNATURE", "LG Objet Collection", "LG UP 가전", "케어용품/소모품"], },
+  //   {name:"제품/소모품", tagName:["TV/AV", "PC/모니터", "주방가전", "생활가전", "에어컨/에어케어", "뷰티/의료기기", "로봇", "LG SIGNATURE", "LG Objet Collection", "LG UP 가전", "케어용품/소모품"], },
+  // ]
+  const navlist = [
+    { name: "제품/소모품" },
+    { name: "렌탈/케어" },
+    { name: "이벤트/멤버십" },
+    { name: "스토리" },
+    { name: "베스트샵" },
+  ];
   return (
     <HeaderMenu>
-      <NavList style={{ paddingLeft: "15px" }}>제품/소모품</NavList>
-      <NavList>고객지원</NavList>
-      <NavList>렌탈/케어</NavList>
-      <NavList>이벤트/멤버십</NavList>
-      <NavList>스토리</NavList>
-      <NavList>베스트샵</NavList>
+      {/* <NavList style={{ paddingLeft: "15px" }}>제품/소모품</NavList> */}
+      {navlist.map((item, index) => (
+        <NavList key={index}>
+          <NavItemTag href="/">{item.name}</NavItemTag>
+          <CategoryLayer>
+            <CategoryInner>
+              <SwiperContainer>
+                <SwiperWrap>
+                  <SwiperSlide>
+                    <SlideItemTag href="/">TV/AV</SlideItemTag>
+                  </SwiperSlide>
+                </SwiperWrap>
+              </SwiperContainer>
+            </CategoryInner>
+          </CategoryLayer>
+        </NavList>
+      ))}
+
       <div
         style={{
           top: "0",
--- src/styles/Header.js
+++ src/styles/Header.js
@@ -45,9 +45,104 @@ export const NavList = styled.li`
   position: relative;
   display: block;
   padding: 4px 15px 4px 0;
+  /* font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  padding-left: 24px; */
+  /* vertical-align: top; */
+`;
+
+export const NavItemTag = styled.a`
+  position: relative;
+  display: block;
+  padding: 4px 0 0;
   font-weight: 600;
   font-size: 16px;
   line-height: 24px;
-  padding-left: 24px;
-  /* vertical-align: top; */
+  padding-left: 12px;
+  vertical-align: top;
+  color: inherit;
+  text-decoration: none;
+`;
+
+export const CategoryLayer = styled.div`
+  display: none;
+`;
+
+export const CategoryInner = styled.div`
+  position: absolute;
+  top: 105px;
+  left: 0;
+  right: 0;
+  z-index: 103;
+  border-top: 1px solid #ddd;
+  background-color: #fff;
+`;
+
+export const SwiperContainer = styled.div`
+  text-align: left;
+  max-width: 1760px;
+  margin: 0 auto 6px;
+  padding: 25px 40px 0;
+  touch-action: pan-y;
+  position: relative;
+  overflow: hidden;
+  list-style: none;
+  z-index: 1;
+  box-shadow: none !important;
+  border-radius: 0 !important;
+`;
+
+export const SwiperWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  transition-property: transform;
+  box-sizing: content-box;
+  transform: translate3d(0, 0, 0);
+`;
+
+// export const SwiperSlideOn = styled.div `
+// margin-left: 0;
+// flex-shrink: 0;
+// display: inline-block;
+// position: relative;
+// width: auto;
+// padding-bottom: 2px;
+// transition-property: transform;
+// transform: translate3d(0,0,0);
+// `
+
+export const SwiperSlide = styled.div`
+  flex-shrink: 0;
+  display: inline-block;
+  position: relative;
+  width: auto;
+  margin-left: 16px;
+  padding-bottom: 2px;
+  transition-property: transform;
+  transform: translate3d(0, 0, 0);
+`;
+
+export const SlideItemTag = styled.a`
+  color: #000;
+  display: inline-block;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 500;
+  text-decoration: none;
+`;
+
+export const Blind = styled.em`
+  position: absolute;
+  z-index: -10;
+  width: 0 !important;
+  height: 0 !important;
+  overflow: hidden !important;
+  font-size: medium;
+  line-height: normal;
+  word-break: break-all;
+  font-style: normal;
 `;
